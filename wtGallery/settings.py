368c44bc390bc595adcfc4f2e616333865f82173
Rajneesh Yadav <yadavrajneesh999@gmail.com>
2020-10-19 14:18:17 +0530

    Update settings.py

--- wtGallery/settings.py
+++ wtGallery/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = ')27e+ut04i=9&wp4!cq73n6ao^2l(xbussag07(l!=e$355vp-'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
